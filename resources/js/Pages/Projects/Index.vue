0a68f987936d923e814610612a437b556da1b3e5
axlkun <axelcruz.dev@gmail.com>
2023-10-18 14:30:40 -0600

    create index view for projects

---
+++ resources/js/Pages/Projects/Index.vue
@@ -0,0 +1,62 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import EditBtn from '@/OwnComponents/EditBtn.vue';
+import DeleteBtn from '@/OwnComponents/DeleteBtn.vue';
+import AppTable from '@/OwnComponents/Table.vue';
+import Container from '@/OwnComponents/Container.vue';
+import Card from '@/OwnComponents/Card.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import BreadCroumbs from '@/OwnComponents/BreadCroumbs.vue';
+
+const props = defineProps({
+    projects: Object,
+});
+
+const headers = [
+    { name: "Name" },
+    { name: "Client" },
+    { name: "Created date" },
+    {
+        name: "Actions",
+        class: "text-right"
+    }
+];
+
+const breadcrumbs = [
+    {
+        label: "Projects"
+    }
+];
+
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <BreadCroumbs :items="breadcrumbs"></BreadCroumbs>
+        </template>
+
+        <Container>
+            <PrimaryButton :href="route('projects.create')">Add new</PrimaryButton>
+
+            <Card class="mt-4">
+                <AppTable :headers="headers" :items="projects">
+                    <tr v-for="project in projects.data" :key="project.id">
+                        <td>{{ project.name }}</td>
+                        <td>{{ project.company }}</td>
+                        <td>{{ project.created_at_formated }}</td>
+                        <td>
+                            <div class="flex items-center justify-end space-x-2">
+                                <EditBtn :url="route('projects.edit', { project: project.id })"></EditBtn>
+
+                                <DeleteBtn :url="route('project.destroy', { project: project.id })"
+                                    module-name="project"></DeleteBtn>
+                            </div>
+                        </td>
+                    </tr>
+                </AppTable>
+            </Card>
+        </Container>
+
+    </AppLayout>
+</template>
