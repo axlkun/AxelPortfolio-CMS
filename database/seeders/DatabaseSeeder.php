2f868153c15a4dcff544057b009a8e522871e23e
axlkun <axelcruz.dev@gmail.com>
2023-10-18 11:55:09 -0600

    set user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\User;
 use App\Models\Article;
 use App\Models\Category;
 use Illuminate\Database\Seeder;
@@ -17,10 +18,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@website.com',
+            'password' => bcrypt('superusuario2000')
+        ]);
 
         // Category::factory(10)->create();
         //Article::factory(15)->create();
