e4b36d2c55639f0df20fdaccf2bb98b849227949
axlkun <axelcruz.dev@gmail.com>
2023-10-19 10:26:42 -0600

    add imageUrl

--- app/Http/Resources/ProjectResource.php
+++ app/Http/Resources/ProjectResource.php
@@ -16,14 +16,14 @@ class ProjectResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'title' => $this->name,
-            'slug' => $this->slug,
-            'image' => $this->image, 
-            'description' => $this->description, 
-            'technologies' => $this->technologies, 
-            'company' => $this->company, 
-            'website_link' => $this->website_link, 
-            'repo_link' => $this->repo_link, 
+            'title' => $this->when($this->title,$this->title),
+            'slug' => $this->when($this->slug,$this->slug),
+            'imageUrl' => $this->getImageUrl(),
+            'description' => $this->when($this->description,$this->description), 
+            'technologies' => $this->when($this->technologies,$this->technologies), 
+            'company' => $this->when($this->company,$this->company), 
+            'website_link' => $this->when($this->website_link,$this->comwebsite_linkpany), 
+            'repo_link' => $this->when($this->repo_link,$this->repo_link), 
             'created_at_formated' => $this->when($this->created_at, function(){
                 return $this->created_at->diffForHumans();
             })
