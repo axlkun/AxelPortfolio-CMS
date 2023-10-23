cff23f21c1f77ccbd351cca7937993cea132956c
axlkun <axelcruz.dev@gmail.com>
2023-10-22 18:30:18 -0600

    add articles relation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function articles(): BelongsToMany
+    {
+        return $this->belongsToMany(Article::class);
+    }
 }
