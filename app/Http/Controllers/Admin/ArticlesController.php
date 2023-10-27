cdc69d2f17f8c55c4bdb78382d5731fe402514ee
axlkun <axelcruz.dev@gmail.com>
2023-10-26 21:54:07 -0600

    return article categories to index view

--- app/Http/Controllers/Admin/ArticlesController.php
+++ app/Http/Controllers/Admin/ArticlesController.php
@@ -16,7 +16,7 @@ class ArticlesController extends Controller
 {
     public function index()
     {
-        $articles = Article::latest()->simplePaginate(10);
+        $articles = Article::with('categories')->latest()->simplePaginate(10);
 
         return Inertia::render('Articles/Index', [
             'articles' => ArticleResource::collection($articles)
