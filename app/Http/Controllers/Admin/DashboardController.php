a24c13c91de1b591995fd9faa5117318e1780abe
axlkun <axelcruz.dev@gmail.com>
2023-10-27 12:59:49 -0600

    send data to dashboard view

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,14 +2,33 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Models\Article;
+use App\Models\Project;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     //
-    public function index(Request $rquest){
-        return Inertia::render('Dashboard');
+    public function index(Request $rquest)
+    {
+        $projectsCount = Project::count();
+        $categoriesCount = Category::count();
+        $articlesCount = Article::count();
+
+        $latestProject = Project::latest('created_at')->value('created_at')->diffForHumans();
+        $latestCategory = Category::latest('created_at')->value('created_at')->diffForHumans();
+        $latestArticle = Article::latest('created_at')->value('created_at')->diffForHumans();
+
+        return Inertia::render('Dashboard', [
+            'projectsCount' => $projectsCount,
+            'categoriesCount' => $categoriesCount,
+            'articlesCount' => $articlesCount,
+            'latestProject' => $latestProject,
+            'latestCategory' => $latestCategory,
+            'latestArticle' => $latestArticle,
+        ]);
     }
 }
