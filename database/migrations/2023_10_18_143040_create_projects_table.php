5bf0b65aaa69f956b8446c91f837c7b59188d611
axlkun <axelcruz.dev@gmail.com>
2023-10-28 19:01:38 -0600

    add summary field

--- database/migrations/2023_10_18_143040_create_projects_table.php
+++ database/migrations/2023_10_18_143040_create_projects_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('slug')->unique();
             $table->string('image');
+            $table->string('summary');
             $table->longText('description');
             $table->string('technologies');
             $table->string('company');
