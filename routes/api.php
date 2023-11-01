a2bd0c163c7c8822e5a0c97b0d859f2d86e00907
axlkun <axelcruz.dev@gmail.com>
2023-10-31 21:05:14 -0600

    endpoint to related articles

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,11 @@ use App\Http\Controllers\Api\ProjectsController;
 use App\Http\Controllers\Api\SettingsController;
 use App\Http\Controllers\Api\ArticlesController;
 use App\Http\Controllers\Api\ContactUsController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('articles',[ArticlesController::class,'index']);
 Route::get('articles/{article:slug}',[ArticlesController::class,'show']);
+Route::get('related-articles/{article:slug}',[ArticlesController::class,'relatedArticles']);
 
 Route::get('projects',[ProjectsController::class,'index']);
 Route::get('projects/{project:slug}',[ProjectsController::class,'show']);
