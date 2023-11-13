17ea46ba37ee41a6025cb3dd65b928b782cedbb8
axlkun <axelcruz.dev@gmail.com>
2023-11-13 14:51:16 -0600

    update current and preview photo

--- resources/js/OwnComponents/Image.vue
+++ resources/js/OwnComponents/Image.vue
@@ -48,13 +48,14 @@ const selectNewPhoto = () => {
 
         <!-- Current Profile Photo -->
         <div v-show="!photoPreview" class="mt-2">
-            <img :src="imageUrl" :alt="imageAlt" class="rounded-full h-20 w-20 object-cover">
+            <img :src="imageUrl" :alt="imageAlt" class="object-cover">
         </div>
 
         <!-- New Profile Photo Preview -->
         <div v-show="photoPreview" class="mt-2">
-            <span class="block rounded-full w-20 h-20 bg-cover bg-no-repeat bg-center"
-                :style="'background-image: url(\'' + photoPreview + '\');'" />
+            <!-- <span class="block w-120 h-80 bg-cover bg-no-repeat bg-center"
+                :style="'background-image: url(\'' + photoPreview + '\');'" /> -->
+                <img :src="photoPreview" :alt="imageAlt" class="object-cover">
         </div>
 
         <SecondaryButton class="mt-2 mr-2" type="button" @click.prevent="selectNewPhoto">
