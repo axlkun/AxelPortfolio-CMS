e88546eac267f0cba1151d016ed663ee27576071
axlkun <axelcruz.dev@gmail.com>
2023-11-18 13:40:00 -0600

    convert images to webp

--- app/Actions/UploadFile.php
+++ app/Actions/UploadFile.php
@@ -4,8 +4,10 @@ namespace App\Actions;
 
 use Illuminate\Support\Str;
 use Illuminate\Http\UploadedFile;
+use Intervention\Image\Facades\Image;
 
-class UploadFile{
+class UploadFile
+{
     private $file;
     private $uploadPath;
 
@@ -13,7 +15,7 @@ class UploadFile{
      * Set the value of file
      *
      * @return  self
-     */ 
+     */
     public function setFile(?UploadedFile $file)
     {
         $this->file = $file;
@@ -25,7 +27,7 @@ class UploadFile{
      * Set the value of uploadPath
      *
      * @return  self
-     */ 
+     */
     public function setUploadPath(string $uploadPath)
     {
         $this->uploadPath = $uploadPath;
@@ -33,21 +35,32 @@ class UploadFile{
         return $this;
     }
 
-    public function execute(): ?string{
+    public function execute(): ?string
+    {
 
-        if(!$this->file){
+        if (!$this->file) {
             return null;
         }
 
         $imageName = (string) Str::of($this->file->getClientOriginalName())
-                ->beforeLast('.')
-                ->slug()
-                ->append('.')
-                ->append($this->file->getClientOriginalExtension());
+            ->beforeLast('.')
+            ->slug()
+            ->append('.')
+            ->append($this->file->getClientOriginalExtension());
 
-        $this->file->storeAs($this->uploadPath,$imageName);
+        $this->file->storeAs($this->uploadPath, $imageName);
         // $this->file->storeAs($this->uploadPath, $imageName, 'public');
 
-        return $imageName;
+        // Verifica si la imagen ya es WebP
+        if (strtolower($this->file->getClientOriginalExtension()) === 'webp') {
+            return $imageName;
+        }
+
+        //convertir a webp
+        $webpImage = Image::make($this->file)->encode('webp', 90);
+        $webpImage->save(storage_path("app/{$this->uploadPath}/" . pathinfo($imageName, PATHINFO_FILENAME) . '.webp'));
+
+        // return $imageName;
+        return pathinfo($imageName, PATHINFO_FILENAME) . '.webp';
     }
 }
