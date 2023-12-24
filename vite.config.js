837683d24a9f84dc8b1c715f75b7f497a25f7500
axlkun <axelcruz.dev@gmail.com>
2023-12-24 14:19:08 -0600

    ckeditor working prod

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import commonjs from "vite-plugin-commonjs";
 
 export default defineConfig({
     plugins: [
@@ -16,5 +17,19 @@ export default defineConfig({
                 },
             },
         }),
+        commonjs({
+            filter(id) {
+              if (["ckeditor5/build/ckeditor.js"].includes(id)) {
+                return true;
+              }
+            },
+          }),
     ],
+
+    optimizeDeps: {
+        include: ["ckeditor5-custom-build"],
+      },
+      build: {
+        commonjsOptions: { exclude: ["ckeditor5-custom-build"] },
+      },
 });
