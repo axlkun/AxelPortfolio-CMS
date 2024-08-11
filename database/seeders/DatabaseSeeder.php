761ae01ca46b5e52c121b33a7d1dcf7081fb9fb5
axlkun <axelcruz.dev@gmail.com>
2024-08-11 12:22:20 -0600

    remove unused code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,12 +18,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@website.com',
-            'password' => bcrypt('superusuario2000')
-        ]);
-
         // Category::factory(10)->create();
         //Article::factory(15)->create();
     }
